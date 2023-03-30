ecc271da0556548a7a31ab8074686f485393a048
GloryKO <koladeg2022@gmail.com>
2023-03-30 14:16:42 -0700

    added data tranformation method

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -36,6 +36,6 @@ class DataIngestion:
 
             return (self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
 
-        except:
-            pass 
+        except Exception as e:
+            raise CustomException(e,sys) 
         
